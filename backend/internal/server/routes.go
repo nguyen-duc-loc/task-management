dc6f9624ecfa287684d3f36ef7f7829fa4874e70
Loc Nguyen <nguyenducloc404@gmail.com>
2025-05-10 15:00:47 +0700

    chore: init backend

---
+++ backend/internal/server/routes.go
@@ -0,0 +1,40 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+var serverEnv string
+
+func (s *Server) RegisterRoutes() http.Handler {
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{fmt.Sprintf("http://localhost:%s", os.Getenv("FRONTEND_PORT"))},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowHeaders:     []string{"Accept", "Authorization", "Content-Type"},
+		AllowCredentials: true, // Enable cookies/auth
+	}))
+
+	r.GET("/", s.HelloWorldHandler)
+
+	r.GET("/health", s.healthHandler)
+
+	return r
+}
+
+func (s *Server) HelloWorldHandler(c *gin.Context) {
+	resp := make(map[string]string)
+	resp["message"] = "Hello World"
+
+	c.JSON(http.StatusOK, resp)
+}
+
+func (s *Server) healthHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, s.db.Health())
+}
